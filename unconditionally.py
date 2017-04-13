6a682cad94335646119754081d806f152ad05b8a
martin-martin <breuss.martin@gmail.com>
2017-04-12 21:27:37 -0400

    change parchment

--- unconditionally.py
+++ unconditionally.py
@@ -72,7 +72,7 @@ def create_speech_for_web(clozed_text, filename):
 			max-width: 800px;
 			font-family: 'Amatic SC', cursive;
 			font-size: 2.3em;
-			background-image: url("https://cdn.pixabay.com/photo/2015/09/22/12/18/paper-951489_960_720.jpg");
+			background-image: url("http://www.publicdomainpictures.net/pictures/70000/velka/old-parchment.jpg");
 		}
 		p {
 			padding: 20px;
@@ -119,7 +119,7 @@ text_POS_tagged = tag_POS(tokenize_text(kennedy2_full))
 # https://www.ling.upenn.edu/courses/Fall_2003/ling001/penn_treebank_pos.html
 # the "essentials" selection keeps all nouns and verbs - nudging to that Unconditional Language Universal
 essentials = cloze_text(text_POS_tagged, "NNP", "NNPS", "NN", "NNS", "VB", "VBD", "VBG", "VBN", "VBP", "VBZ")
-# However, this amounts still to quite some words. Because I didn't want to read so much, 
+# However, this amounts still to quite some words. Because I didn't want to read so much,
 # I also tried a version with stripping the text down to only proper nouns. It's fun 😁
 noun_speak = cloze_text(text_POS_tagged, "NNP", "NNPS")
 
@@ -129,4 +129,71 @@ noun_speak = cloze_text(text_POS_tagged, "NNP", "NNPS")
 ################################ CHOOSE YOUR OWN ################################
 
 # enter a president's speech file name to create a small website with the essential words
+# here are the (many) choices
+# '1945-Truman.txt'
+# '1946-Truman.txt'
+# '1947-Truman.txt'
+# '1948-Truman.txt'
+# '1949-Truman.txt'
+# '1950-Truman.txt'
+# '1951-Truman.txt'
+# '1953-Eisenhower.txt'
+# '1954-Eisenhower.txt'
+# '1955-Eisenhower.txt'
+# '1956-Eisenhower.txt'
+# '1957-Eisenhower.txt'
+# '1958-Eisenhower.txt'
+# '1959-Eisenhower.txt'
+# '1960-Eisenhower.txt'
+# '1961-Kennedy.txt'
+# '1962-Kennedy.txt'
+# '1963-Johnson.txt'
+# '1963-Kennedy.txt'
+# '1964-Johnson.txt'
+# '1965-Johnson-1.txt'
+# '1965-Johnson-2.txt'
+# '1966-Johnson.txt'
+# '1967-Johnson.txt'
+# '1968-Johnson.txt'
+# '1969-Johnson.txt'
+# '1970-Nixon.txt'
+# '1971-Nixon.txt'
+# '1972-Nixon.txt'
+# '1973-Nixon.txt'
+# '1974-Nixon.txt'
+# '1975-Ford.txt'
+# '1976-Ford.txt'
+# '1977-Ford.txt'
+# '1978-Carter.txt'
+# '1979-Carter.txt'
+# '1980-Carter.txt'
+# '1981-Reagan.txt'
+# '1982-Reagan.txt'
+# '1983-Reagan.txt'
+# '1984-Reagan.txt'
+# '1985-Reagan.txt'
+# '1986-Reagan.txt'
+# '1987-Reagan.txt'
+# '1988-Reagan.txt'
+# '1989-Bush.txt'
+# '1990-Bush.txt'
+# '1991-Bush-1.txt'
+# '1991-Bush-2.txt'
+# '1992-Bush.txt'
+# '1993-Clinton.txt'
+# '1994-Clinton.txt'
+# '1995-Clinton.txt'
+# '1996-Clinton.txt'
+# '1997-Clinton.txt'
+# '1998-Clinton.txt'
+# '1999-Clinton.txt'
+# '2000-Clinton.txt'
+# '2001-GWBush-1.txt'
+# '2001-GWBush-2.txt'
+# '2002-GWBush.txt'
+# '2003-GWBush.txt'
+# '2004-GWBush.txt'
+# '2005-GWBush.txt'
+# '2006-GWBush.txt'
+
 get_real('1962-Kennedy.txt')
